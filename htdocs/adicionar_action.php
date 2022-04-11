4f375df8109ec75ccb69111c9da5e2896c0b65d4
joao-vitorsantos <55136727+joao-vitorsantos@users.noreply.github.com>
2022-04-11 13:36:11 -0400

    Add files via upload

--- htdocs/adicionar_action.php
+++ htdocs/adicionar_action.php
@@ -6,17 +6,23 @@
 
     if($name && $email){
 
-        $sql->prepare("SELECT * FROM usuarios WHERE email = :email");
+        $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
         $sql->bindValue(':email', $email);
         $sql->execute();
-        
-        $sql = $pdo->prepare("INSERT INTO usuarios (nome, email) VALUES (:name, :email)");
-        $sql->bindValue(':name', $name);
-        $sql->bindParam(':email', $email);
-        $sql->execute();
 
-        header("Location: index.php");
-        exit;
+
+        if($sql->rowCount() === 0){
+            $sql = $pdo->prepare("INSERT INTO usuarios (nome, email) VALUES (:name, :email)");
+            $sql->bindValue(':name', $name);
+            $sql->bindParam(':email', $email);
+            $sql->execute();
+        
+            header("Location: index.php");
+            exit;
+        } else {
+            header("Location: adicionar.php");
+            exit;
+        }
 
     }else {
         header("Location: adicionar.php");
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,11 @@
 <?php
     require 'config.php';
+
+    $lista = [];
+    $sql = $pdo->query("SELECT * FROM usuarios");
+    if($sql->rowCount() > 0){
+        $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
 ?>
 
     <a href="adicionar.php">ADICIONAR USUÁRIO</a>
@@ -11,4 +17,14 @@
             <th>EMAIL</th>
             <th>AÇÕES</th>
         </tr>
+            <?php foreach($lista as $usuario): ?>
+                <tr>
+                    <td><?php echo $usuario['id']; ?></td>
+                    <td><?php echo $usuario['nome']; ?></td>
+                    <td><?php echo $usuario['email']; ?></td>
+                    <td>
+                        <a href="editar.php?id=<?=$usuario['id'];?>">[ Editar ]</a>
+                        <a href="excluir.php">[ Excluir ]</a>
+                </tr>
+            <?php endforeach; ?>
     </table>
